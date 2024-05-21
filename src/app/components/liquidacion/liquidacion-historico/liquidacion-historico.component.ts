d51d6541a778798300742c95ab8b96a35ca904a5
Chopperkasa <limaszandresfelipe@gmail.com>
2024-05-21 17:29:20 -0500

    feat: udistrital/sga_documentacion#225 Refactorización del duncionamiento de la tabla de liquidaciones en liquidacion-historico

--- src/app/components/liquidacion/liquidacion-historico/liquidacion-historico.component.ts
+++ src/app/components/liquidacion/liquidacion-historico/liquidacion-historico.component.ts
@@ -14,25 +14,6 @@ import { SgaMidService } from 'src/app/services/sga_mid.service';
   styleUrls: ['./liquidacion-historico.component.scss']
 })
 export class LiquidacionHistoricoComponent {
-
-  // proyectos=[
-  //   { "Id": 1, "Nombre": "Proyecto 1" },
-  //   { "Id": 2, "Nombre": "Proyecto 2" },
-  //   { "Id": 3, "Nombre": "Proyecto 3" },
-  // ]
-
-  // nivel_load =[
-  //   { "Id": 1, "Nombre": "Nivel 1" },
-  //   { "Id": 2, "Nombre": "Nivel 2" },
-  //   { "Id": 3, "Nombre": "Nivel 3" }
-  // ]
-
-  // periodos=[
-  //   { "Nombre": "Periodo 1" },
-  //   { "Nombre": "Periodo 2" },
-  //   { "Nombre": "Periodo 3" }
-  // ]
-
   datosTabla: any;
   tablahistorico:boolean=true
 
@@ -41,11 +22,6 @@ export class LiquidacionHistoricoComponent {
   periodoControl = new FormControl('', [Validators.required]);
   Campo2Control = new FormControl('', [Validators.required]);
   Campo4Control = new FormControl('', [Validators.required]);
-  // firstFormGroup = this._formBuilder.group({
-  //   validatorProyecto: ['', Validators.required],
-  //   validatorPeriodo: ['', Validators.required],
-  //   validatorFacultad: ['', Validators.required],
-  // });
 
   facultades!: any[]
   proyectosCurriculares!: any[]
@@ -81,7 +57,6 @@ export class LiquidacionHistoricoComponent {
       this.oikosService.get('dependencia_padre/FacultadesConProyectos?Activo:true&limit=0')
         .subscribe((res: any) => {
           this.facultades = res;
-          console.log(this.facultades);
           resolve(res)
         },
           (error: any) => {
@@ -102,7 +77,6 @@ export class LiquidacionHistoricoComponent {
       this.parametrosService.get('periodo/?query=CodigoAbreviacion:PA&sortby=Id&order=desc&limit=0')
         .subscribe((res: any) => {
           this.periodos = res.Data;
-          console.log(this.periodos);
           resolve(res)
         },
           (error: any) => {
@@ -119,34 +93,12 @@ export class LiquidacionHistoricoComponent {
     const periodo = this.periodoControl.value;
 
     this.inscripciones = await this.buscarInscripcionesAdmitidosLegalizados(proyecto, periodo)
-    console.log("INSCRIPCIONES", this.inscripciones)
-
-    // this.datosTabla["inscripciones"] = this.inscripciones
-    // this.datosTabla["editando"] = true
 
     this.datosTabla = {
       "inscripciones": this.inscripciones,
       "visible": true
     }
     this.loading = false;
-    // for (const inscripcion of this.inscripciones) {
-    //   const persona: any = await this.consultarTercero(inscripcion.PersonaId);
-    //   if (Array.isArray(persona) && persona.length === 0) {
-    //     continue;
-    //   }
-    //   const infoLegalizacion = await this.getLegalizacionMatricula(persona.Id)
-    //   if (infoLegalizacion == "No existe legalizacion") {
-    //     continue;
-    //   }
-    //   console.log("INSCRIP:", inscripcion, persona, infoLegalizacion);
-
-    //   const valorLiquidacion = {
-    //     "codigo": 1000,
-    //     "documento": persona.NumeroIdentificacion,
-    //     "nombres": persona.PrimerNombre + "" + persona.SegundoNombre,
-    //     "apellidos": persona.PrimerApellido + "" + persona.SegundoApellido,
-    //   }
-    // }
   }
 
   buscarInscripcionesAdmitidosLegalizados(proyecto: any, periodo: any) {
@@ -162,31 +114,4 @@ export class LiquidacionHistoricoComponent {
           });
     });
   }
-
-  // async consultarTercero(personaId: any): Promise<any | []> {
-  //   try {
-  //     const response = await this.sgamidService.get('persona/consultar_persona/' + personaId).toPromise();
-  //     return response;
-  //   } catch (error) {
-  //     // this.popUpManager.showErrorAlert(this.translate.instant('legalizacion_matricula.tercero_error'));
-  //     console.log(error);
-  //     return []; // Return an empty array to indicate an error
-  //   }
-  // }
-
-  // async getLegalizacionMatricula(personaId: any) {
-  //   return new Promise((resolve, reject) => {
-  //     //this.loading = true;
-  //     this.inscripcionMidService.get('legalizacion/informacion-legalizacion/' + personaId)
-  //       .subscribe((res: any) => {
-  //         resolve(res.data);
-  //       },
-  //         (error: any) => {
-  //           this.popUpManager.showErrorAlert(
-  //             this.translate.instant('admision.legalizacion_error')
-  //           );
-  //         });
-  //   });
-  // }
-
 }
