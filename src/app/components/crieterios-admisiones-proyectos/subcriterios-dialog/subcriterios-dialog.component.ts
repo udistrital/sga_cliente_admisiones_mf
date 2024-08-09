eb0b73b293ceacbcc9ad2392a817d7fadd034132
Bryan Munoz <swsbmm@gmail.com>
2024-08-09 08:37:55 -0500

    feat: nuevo componente para subcriterios udistrital/sga_documentacion#388

---
+++ src/app/components/crieterios-admisiones-proyectos/subcriterios-dialog/subcriterios-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subcriterios-dialog',
+  templateUrl: './subcriterios-dialog.component.html',
+  styleUrls: ['./subcriterios-dialog.component.scss']
+})
+export class SubcriteriosDialogComponent {
+
+}
