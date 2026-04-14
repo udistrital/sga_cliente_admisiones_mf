6677188931554b4f35eedf1f4f7c7cc8b6ce2f2e
Alvaro Zarabanda <alvaroalejo25@gmail.com>
2026-04-14 14:12:10 -0500

    fix: actualizacion de sga_cliente_admisiones_mf udistrital/sga_documentacion#695
    se ajusta query de periodo en criterio admision para obtenerlos en orden

--- src/app/components/crieterios-admisiones-proyectos/criterio_admision/criterio_admision.component.ts
+++ src/app/components/crieterios-admisiones-proyectos/criterio_admision/criterio_admision.component.ts
@@ -263,7 +263,7 @@ export class CriterioAdmisionComponent implements OnChanges {
   cargarPeriodo() {
     return new Promise((resolve, reject) => {
       this.parametrosService
-        .get("periodo?query=CodigoAbreviacion:PA&sortby=Id&order=desc&limit=0")
+        .get("periodo?query=CodigoAbreviacion:PA&sortby=Nombre&order=desc&limit=0")
         .subscribe(
           (res: any) => {
             const r = <any>res;
